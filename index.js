8f6c33b7ceea9e57538a557c6244e0c741df7ea6
Mythical Labs <mythicallabsremote@gmail.com>
2024-12-12 17:39:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const fs = require('fs')
 const app = express();
 const path = require('path')
 let pagesPath = path.join(process.cwd(), '/pages/index.html');
-app.get('/', (req, res) => res.send(fs.readFileSync(pagesPath)));
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'text/html');
+    res.send(fs.readFileSync(pagesPath))
+});
 
 const port = process.env.PORT || 3000;
 
