cbc8a29b72b8c18b5c1a9f337bc9c1bfc2bd819f
Mythical Labs <mythicallabsremote@gmail.com>
2024-12-12 17:48:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,17 @@ app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
     res.send(fs.readFileSync(pagesPath))
 });
-
+app.get('/lib/:filetype/:filename', (req, res) => {
+    let filename = req.params.filename;
+    let filetype = req.params.filetype;
+    let filePath = path.join(process.cwd(), `/lib/${filename}`);
+    if(filetype == 'js'){
+        res.set('Content-Type', 'application/javascript');
+    }else{
+        res.set('Content-Type', 'text/css');
+    }
+    res.send(fs.readFileSync(filePath))
+})
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
