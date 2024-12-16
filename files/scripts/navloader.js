b0553b89e1f4ff95caafc0f3301235500a38676b
Mythical Labs <mythicallabsremote@gmail.com>
2024-12-16 09:07:03 -0600

    V0.3.5 - Bug Fix
    Fixed navloader.js preventing page load

--- files/scripts/navloader.js
+++ files/scripts/navloader.js
@@ -1,10 +1,6 @@
 async function addNav(){
     var navId = "a#nav" + window.location.href.split('/').pop();
     $("#navout").load(`/navbar/${navId} .nav`);
-    for(i=0;i<50;){
-        setTimeout(setactivenav(), 100)
-    }
-    setTimeout(setactivenav(), 300)
 }
 function setactivenav(){
     var navId = "a#nav" + window.location.href.split('/').pop();
