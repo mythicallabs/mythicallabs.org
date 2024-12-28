5f3c20d5e36004df99845ddf91e8ce6fb79ee0ee
Mythical Labs <mythicallabsremote@gmail.com>
2024-12-27 18:57:00 -0600

    V1.6.5[DEV]

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ app.get('/favicon.ico', (req, res) => {
     res.set('Content-Type', 'image/x-icon');
     res.send(fs.readFileSync(path.join(process.cwd(), '/favicon.ico')))
 })
-app.get('/pay', (req, res) => {
-    res.set('Content-Type', 'text/html');
-    res.send(`<html><script>window.location.href = '/couldhavebeenhacked'</script></html>`);
-})
 app.get('/devmsg', (req, res) => {
     from = req.query.from;
         const filePath = path.join(process.cwd(), '/files/pages/base/devmsg.html');
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (err) {
                 res.set('Content-Type', 'text/html')
-                deliver404error(req, res, err, 'Go to', 'projects')            
+                deliver404error(req, res, err, 'Back to', 'projects')            
             }
             let modifiedData = data.replace(`<button class="button" onclick="window.location.href = '/about'">Back to</button>`, `<button class="button" onclick="window.location.href = '/${from}'">Back to ${from}</button>`);
             res.set('Content-Type', 'text/html');
             res.send(modifiedData);
         });
+})
+app.get('/photography', (req, res) => {
+    try{
+        res.set('Content-Type', 'text/html')
+        res.send(fs.readFileSync(path.join(process.cwd(), '/files/photography/pages/index.html')))
+    }catch(err){
+        res.set('Content-Type', 'text/html')
+        deliver404error(req, res, err, 'Back to', 'homepage')
+    }
 })
 app.get('/:page', (req, res) => {
     let page = req.params.page;
@@ -34,7 +39,7 @@ app.get('/:page', (req, res) => {
             res.send(fs.readFileSync(path.join(process.cwd(), '/files/pages/base/', page +'.html')));
         }catch(err){
             res.set('Content-Type', 'text/html');
-            deliver404error(req, res, err, 'Go', 'home')
+            deliver404error(req, res, err, 'Back to', 'homepage')
         }
 });
 app.get('/projects/:projectname/:projectpage', (req, res) => {
@@ -46,6 +51,35 @@ app.get('/projects/:projectname/:projectpage', (req, res) => {
     try{
         res.set('Content-Type', 'text/html')
         res.send(fs.readFileSync(path.join(process.cwd(), '/files/pages/projects/' + projectname + '/' + projectpage + '.html')))
+    }catch(err){
+        res.set('Content-Type', 'text/html')
+        deliver404error(req, res, err, 'Back to', 'projects')
+    }
+})
+app.get('/photography/:page', (req, res) => {
+    const page = req.params.page
+    if(page == ''){
+        projectpage = 'index'
+    }
+    try{
+        res.set('Content-Type', 'text/html')
+        res.send(fs.readFileSync(path.join(process.cwd(), '/files/photography/pages/' + page + '.html')))
+    }catch(err){
+        res.set('Content-Type', 'text/html')
+        deliver404error(req, res, err, 'Back to', 'photography')
+    }
+})
+app.get('/photography/photo/:photo', (req, res) => {
+    const photo = req.params.photo
+    if(photo == ''){
+        res.set('Content-Type', 'text/html')
+        deliver404error(req, res, err, 'Back to', 'photography', 'We couldnt find that photo')
+    }
+    try{
+        res.set('Content-Type', 'text/html')
+        if(type == 0){
+            res.send(fs.readFileSync(path.join(process.cwd(), '/files/photography/photos/' + photo + type)))
+        }
     }catch(err){
         res.set('Content-Type', 'text/html')
         deliver404error(req, res, err, 'Back to', 'projects')
@@ -81,16 +115,21 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
 
-function deliver404error(req, res, err, message, backto){
-    //assuming that the homepage is located at /
-    if(backto == "home"){
+function deliver404error(req, res, err, message, backto, custommessage){
+    //assuming that the homepage is located at 
+    let pnfmessage = `Oops, looks like that page dosen't exsits.`
+    if(custommessage){
+        pnfmessage = custommessage;
+    }
+    if(backto == "homepage"){
         pagepath = ''
     }else{
         pagepath = backto
     }
     const filePath = path.join(process.cwd(), '/files/pages/base/404.html');
     fs.readFile(filePath, 'utf8', (err, data) => {
-        let modifiedData = data.replace(`<button class="button" onclick="window.location.href = '/about'">Back to</button>`, `<button class="button" onclick="window.location.href = '/${pagepath}'">${message} ${backto}</button>`);
-        return res.send(modifiedData);
+        data = data.replace(`<button class="button" onclick="window.location.href = '/about'">[BACKTO]</button>`, `<button class="button" onclick="window.location.href = '/${pagepath}'">${message} ${backto}</button>`);
+        data = data.replace(`<p>[MESSAGE]</p>`, `<p>${pnfmessage}</p>`)
+        return res.send(data);
     });
 }
