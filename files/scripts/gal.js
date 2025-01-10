c35bd82401e279c8166e7a4639aa20d74bb3214e
Mythical Labs <mythicallabsremote@gmail.com>
2025-01-10 10:45:14 -0600

    testing

--- files/scripts/gal.js
+++ files/scripts/gal.js
@@ -6,7 +6,7 @@ async function a(){
     .then(function(json) {
         for(i=1; json.server.amount >= i; i++){
             let org = document.getElementById('photosout').innerHTML
-            let mod = old + `<img src='/photography/photo/${i}.jpg'>`
+            let mod = org + `<img src='/photography/photo/${i}.jpg'>`
             org = mod
         }
     });
