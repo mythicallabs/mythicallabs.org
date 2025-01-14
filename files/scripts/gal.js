74d53ae6bc65128a06cd589b9dd9c1ef2607977f
Mythical Coding Labs <mythicallabsremote@gmail.com>
2025-01-14 09:31:06 -0600

    Gallery Update

--- files/scripts/gal.js
+++ files/scripts/gal.js
@@ -1,10 +1,27 @@
+var p = 1;
 async function a(){
     await fetch('/pdata', { 
         method: 'GET'
     })
     .then(function(response) { return response.json(); })
     .then(function(json) {
-        for(i=1; json.server.amount >= i; i++){
+        for(i=json.server.amount; 1 <= i; i--){
+            let org = document.getElementById('photosout').innerHTML
+            let mod = org + `<img src='/photography/photo/${i}.jpg' class='galleryimage'>`
+            document.getElementById('photosout').innerHTML = mod
+        }
+    });
+}
+async function frontone(){
+    p++
+    document.getElementById('photosout').innerHTML = "";
+    document.getElementById('pageno').innerHTML = p;
+    await fetch('/pdata', { 
+        method: 'GET'
+    })
+    .then(function(response) { return response.json(); })
+    .then(function(json) {
+        for(i=json.server.amount-(p*10); p*10 <= i; i--){
             let org = document.getElementById('photosout').innerHTML
             let mod = org + `<img src='/photography/photo/${i}.jpg' class='galleryimage'>`
             document.getElementById('photosout').innerHTML = mod
