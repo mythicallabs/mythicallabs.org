dfb0c6fc41572a3d364371aa9838b65a25463b2e
Mythical Labs <mythicallabsremote@gmail.com>
2025-01-30 11:17:03 -0600

    1.7.3

--- index.js
+++ index.js
@@ -114,15 +114,10 @@ app.get('/photography/photo/:photo', async (req, res) => {
                 deliver404error(req, res, err, 'Back to', 'photography', `There was an error loading that photo`);
             }
         }else if(htl == 'rendered'){
-            try{
-                const orgpage = await fs.readFileSync(path.join(process.cwd(), '/files/photography/pages/renderedphoto.html'))
-                const newpage = orgpage.replace('<IMAGEPLACEHOLDER>', `<img src="/photography/photo/${photo}">`)
-                res.set('Content-Type', 'text/html');
-                res.send(newpage)
-            }catch(err){
-                res.set('Content-Type', 'text/html');
-                deliver404error(req, res, err, 'Back to', 'photography', 'There was an error loading that photo')
-            }
+            const orgpage = fs.readFileSync(path.join(process.cwd(), '/files/photography/pages/renderedphoto.html'))
+            const newpage = orgpage.replace('<IMAGEPLACEHOLDER>', `<img src="/photography/photo/${photo}">`)
+            res.set('Content-Type', 'text/html');
+            res.send(newpage)
         }
     }else{
         try {
