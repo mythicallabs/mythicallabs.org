9d7896b243d9a59312977fa03c45046e78d1f555
Mythical Labs <mythicallabsremote@gmail.com>
2025-02-06 09:08:28 -0600

    V1.8.0
    Mobile navbar arrow added for useability
    Gallery load method finished
    Projects have a navbar
    Navbar saves position on mobile

--- files/scripts/navloader.js
+++ files/scripts/navloader.js
@@ -3,26 +3,44 @@
 //(c)Rainier Hughes 2024
 async function addNav(){
     if(window.location.href.includes('project?p=')){
-        await $("#navout").load(`/navbar .nav`);
+        await $("#navout").load(`/navbar`);
     }else{
-    let navId = "#nav" + window.location.href.split('/').pop();
-    if(navId == '#nav'){
-        navId = '#navhome';
+        let navId = "#nav" + window.location.href.split('/').pop();
+        if(navId == '#nav'){
+            navId = '#navhome';
+        }
+        await $("#navout").load(`/navbar`);
+        document.head.innerHTML = document.head.innerHTML + `<style>
+        ${navId}{
+            float: left;
+            padding: 20px 30px 10px 30px;
+            height: 40px;
+            font-size: 1.5em;
+            text-decoration: none;
+            background-color: #3a4add;
+        }
+        ${navId}:hover{
+            background-color: #3a4add;
+        }
+        </style>`;
     }
-    await $("#navout").load(`/navbar .nav`);
-    document.head.innerHTML = document.head.innerHTML + `<style>
-    ${navId}{
-        float: left;
-        padding: 20px 30px 10px 30px;
-        height: 40px;
-        font-size: 1.5em;
-        text-decoration: none;
-        background-color: #3a4add;
-    }
-    ${navId}:hover{
-        background-color: #3a4add;
-    }
-    </style>`
 }
+async function hideNav() {
+    document.querySelector(".navarrow.right").style.display = 'none';
 }
-addNav()
+function updatenavpos(){
+    var navElement = document.querySelector('.nav');
+    if (navElement) {
+        window.addEventListener('beforeunload', function() {
+            localStorage.setItem('navScrollPosition', navElement.scrollLeft);
+        });
+
+        var scrollPosition = localStorage.getItem('navScrollPosition');
+        if (scrollPosition) {
+            navElement.scrollLeft = parseInt(scrollPosition);
+        }
+    } else {
+        console.error('Element .nav not found');
+    }
+}
+addNav();
